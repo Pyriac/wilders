18c90a96b8cfdeeb3f22dde99a5f393202be8fa9
Pyriac <cyriacpineau@gmail.com>
2024-06-04 11:45:52 +0200

    add a randomizer for the whole promotion

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import chalk from 'chalk';
 
-console.log(chalk.blue('Baptise'));
-console.log(chalk.red('Sarah'));
-console.log(chalk.green('Ewan'));
-console.log(chalk.yellow('Charlotte'));
+const wilders = ['Alex', 'Anaïs', 'Anthony', 'Antoine', 'Aïssatou', 'Baptiste', 'Benjamin', 'Charlotte', 'Christian', 'Clement', 'Cyriac', 'Damien',
+    'Daniel', 'David', 'Davido', 'Dijan', 'Ewan', 'Geoffrey', 'Guillaume', 'Mathieu', 'Monica', 'Rosa', 'Samih', 'Sarah']
+
+const colors = [chalk.blue, chalk.red, chalk.green, chalk.yellow]
+
+
+colors.forEach(color => {
+    let wilder = wilders[(Math.floor(Math.random() * wilders.length))]
+    console.log(color(wilder));
+});
+
+// console.log(chalk.blue('Baptise'));
+// console.log(chalk.red('Sarah'));
+// console.log(chalk.green('Ewan'));
+// console.log(chalk.yellow('Charlotte'));
